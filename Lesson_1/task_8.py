36f3c4dce14c6638ea7f72de674b9980d3155453
antonbashlovka <anton.bashlovka@gmail.com>
2023-04-18 16:35:02 +0500

    done task 8

--- Lesson_1/task_8.py
+++ Lesson_1/task_8.py
@@ -2,3 +2,20 @@
 #если разрешается сделать один разлом по прямой между дольками 
 #(то есть разломить шоколадку на два прямоугольника).
 
+length = int(input("Введите длину шоколадки "))
+width = int(input("Введите ширину шоколадки "))
+
+k = int(input("Введите количество долек, которые хотите отломить "))
+
+s = length*width
+
+possible = "no"
+r = range(1, k)
+if k < s:
+    for a in r:
+        if k/a == length or k/a == width or a == length or a == width:
+            possible = "yes"
+
+
+
+print(possible)
